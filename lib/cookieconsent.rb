0e19622ad5f6efa75f3739e6106ad9300950b89d
Florian Mientjes <florian@mangrove.nl>
2015-04-10 13:22:22 +0200

    feat(bar): add consent bar

--- lib/cookieconsent.rb
+++ lib/cookieconsent.rb
@@ -12,6 +12,17 @@ module Cookieconsent
       buffer += "</div>".html_safe
     end
   end
+
+  def cookie_consent_bar
+    cookies['cookie-consent'] = params['cookie-consent'] unless params['cookie-consent'].nil?
+    unless cookies['cookie-consent'] == 'yes' || cookies['cookie-consent'] == 'no'
+      buffer = "<div class='cookieconsentbar'>".html_safe
+      buffer += "<p>#{I18n.t('cookieconsent.bar.no-consent')}</p>".html_safe
+      buffer += link_to I18n.t('cookieconsent.bar.no-consent-link'), url_for('cookie-consent' => 'yes'), {class: "cookieconsent-link-allow"}
+      buffer += link_to I18n.t('cookieconsent.bar.no-consent-link-deny'), url_for('cookie-consent' => 'no'), {class: "cookieconsent-link-deny"}
+      buffer += "</div>".html_safe
+    end
+  end
 end
 
 ActionView::Base.send :include, Cookieconsent
